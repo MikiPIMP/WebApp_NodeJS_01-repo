f7d8b5800d29d1d66f1ddc6fd0f3aae6c2a05455
Milan Minic <milan.mm.minic@gmail.com>
2022-02-12 18:20:32 +0100

    Made some changes to the main app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  getHello(): string {
-    return 'Ovo je prva putanja: ROOT';
-  }
-
-  @Get('world')
-  getWorld(): string {
-    return 'Ovo je druga putanja';
+  getIndex(): string {
+    return 'Home page!';
   }
 }
